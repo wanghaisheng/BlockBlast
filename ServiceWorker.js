44f8cc2596b5788ba29783535b9a8bdd76535a68
reunbozdo <95679112+reunbozdo@users.noreply.github.com>
2023-09-27 09:49:24 +0400

    asdf

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "reun-Block Blast Puzzle-0.8";
+const contentToCache = [
+    "Build/BlockBlast.loader.js",
+    "Build/BlockBlast.framework.js.unityweb",
+    "Build/BlockBlast.data.unityweb",
+    "Build/BlockBlast.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
